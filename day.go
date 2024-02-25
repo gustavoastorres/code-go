07e64d4362aac23bffb1ba2224a4d7ef288cebda
Gustavo Torres <gustavoastorres@outlook.com>
2024-02-25 00:59:19 +0000

    learning GO

---
+++ day.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Monday?")
+	today := time.Now().Weekday()
+	switch time.Monday {
+	case today:
+		fmt.Println("Today.")
+	case (today + 1) % 7:
+		fmt.Println("Tomorrow.")
+	case (today + 2) % 7:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
